797c955e3b2edfee1d3b6c577271204e8cccac3b
Brendan <badrysdale01@gmail.com>
2023-08-02 00:35:12 -0400

    updated header with functionality and styles

--- src/components/Header Page/Header.js
+++ src/components/Header Page/Header.js
@@ -1,11 +1,31 @@
 import styled from 'styled-components';
+import { Link } from 'react-router-dom';
+import menu from './icons/menu.png';
 
 const Header = () => {
   return (
     <Container>
-      <div>Header</div>
-      <div>Header2</div>
-      <div>Header3</div>
+      <div>Brendan's Portfolio</div>
+      <List>
+        <li>
+          <Link to="/projects" style={linkStyle}>
+            Projects
+          </Link>
+        </li>
+        <li>
+          <Link to="/about" style={linkStyle}>
+            About
+          </Link>
+        </li>
+        <li>
+          <Link to="/contact" style={linkStyle}>
+            Contact
+          </Link>
+        </li>
+      </List>
+      <Link to="/" style={linkStyle}>
+        <img src={menu} width="15px" height="15px" />
+      </Link>
     </Container>
   );
 };
@@ -14,6 +34,20 @@ const Container = styled.div`
   display: flex;
   justify-content: space-between;
   height: 60px;
+  padding: 10px;
 `;
+const List = styled.ul`
+  list-style-type: none;
+  display: flex;
+  justify-content: space-between;
+  width: 300px;
+  text-decoration: none;
+`;
+
+const linkStyle = {
+  cursor: 'default',
+  textDecoration: 'none',
+  color: 'white',
+};
 
 export { Header };
