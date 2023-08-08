77362c474b03a898e71652ea3d324b0091d6bf9a
Brendan <badrysdale01@gmail.com>
2023-08-08 18:08:34 -0400

    added content to about page

--- src/components/About Page/aboutcontent.js
+++ src/components/About Page/aboutcontent.js
@@ -6,6 +6,26 @@ const aboutcontent = [
     content:
       "I am a highly adaptable technology enthusiast with 8 years of experience spanning hardware maintenance to software development. I enjoy colaborating with many people while promoting a friendly and productive work environment. I've learned a lot in my journey so far and I cant wait to learn more.",
   },
+  {
+    id: 'beginning',
+    type: 'text',
+    title: 'Humble beginnings',
+    content:
+      "I started out as just a young teenager interested in the concept of building a computer. Since then, I've deep dived into as many advanced technology concepts as I could, from the manufacturing of every component in a computer to the creation of the elaborate web applications we all know and love.",
+  },
+  {
+    id: 'education',
+    type: 'gallery',
+    title: 'Experience + Education',
+    content: "I've even gathered a couple of formal diploma's along the way. ",
+    images: [
+      {
+        name: 'Information Technology + Networking',
+        src: 'Insert Diplomas Here',
+      },
+      { name: 'Full Stack Web Development', src: 'Insert Diploma Here' },
+    ],
+  },
 ];
 
 export { aboutcontent };
